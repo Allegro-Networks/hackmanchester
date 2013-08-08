f21738957d147762cb4aed774bde6488c969d82d
Ben Flowers <ben.j.flowers@gmail.com>
2013-08-08 14:03:47 +0100

    rendering html on route

--- website.js
+++ website.js
@@ -1,10 +1,28 @@
-var express = require("express");
-  var app = express();
+var express = require('express'),
+	path = require('path');
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+var website = express(),
+    ROOT_DIRECTORY = __dirname,
+    ASSETS_DIRECTORY = path.join(ROOT_DIRECTORY, 'public'),
+    PAGE_DIRECTORY = path.join(ROOT_DIRECTORY, 'views');
+
+website.set('views', PAGE_DIRECTORY);
+website.set('view engine', 'ejs');
+website.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+website.use(express.bodyParser());
+website.use(express.methodOverride());
+website.use(express.cookieParser('JIMMY'));
+website.use(express.session());
+website.use(express.bodyParser());
+website.use(website.router);
+website.use(express.static(ASSETS_DIRECTORY));
+
+
+website.get('/',function(request,response){
+	response.render('index');
+})
+
+
+website.listen(process.env.PORT,function(){
+    console.log("hack manchester has started on port ",process.env.PORT);
 });
-
-app.listen();
-console.log('Express server started');
-
