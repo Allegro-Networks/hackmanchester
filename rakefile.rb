67971511fe5c35068519f065d95fb05f6349848a
ruby_gem <gemma.cameron@gmail.com>
2014-08-07 10:12:29 +0100

    adding a sponsor us section

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,4 @@
 task :default => [:git]
-multitask :tests => [:unit_tests]
 multitask :dependencies => [:node_dependencies, :ruby_dependencies]
 @run_options = {verbose: Rake.application.options.trace}
 NYAN_REPORTER = 'nyan'
@@ -24,19 +23,10 @@ task :git => :ruby_dependencies do
 	git.push
 end
 
-task :unit_tests do
-	test_format = 'qunit'
-	mocha("./tests/unit",NYAN_REPORTER,test_format)
-end
-
 def committing_code?
 	ENV['m'] != nil
 end
 
 def rake_sh(command)
 	sh command, @run_options
-end
-
-def mocha(test_location,reporter,test_format)
-	rake_sh "mocha #{test_location}  --ui #{test_format} --recursive --reporter #{reporter}"	
 end
