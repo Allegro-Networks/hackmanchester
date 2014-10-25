a3c0483550fec08e84326d2951184a95c9f39fd2
rubygem <gemma.cameron@gmail.com>
2014-10-25 07:38:13 +0100

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = function(website){
 	website.get('/',function(request,response){
 		var headlineSponsor = {uri:'http://www.waukta.com/', name: 'WA:UK TA'}
 		var sponsors = {headline:headlineSponsor},
-			date = moment().format('DD/MM/YYYY'),
+			date = moment().format(),
 			hackEvent = 'Day 1';
 		var model = {sponsors:sponsors, hackEvent:hackEvent};
 		response.render('index', model);
