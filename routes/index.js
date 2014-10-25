dc63a98e8ef5b9c298957e4b5ee6fd9f6951f0b5
rubygem <gemma.cameron@gmail.com>
2014-10-25 07:33:35 +0100

    adding video and date

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var moment = require('moment');
 module.exports = function(website){		
 	website.get('/',function(request,response){
 		var headlineSponsor = {uri:'http://www.waukta.com/', name: 'WA:UK TA'}
-		var sponsors = {headline:headlineSponsor};
-		date = moment().format('DD/MM/YYYY');
-		var model = {sponsors:sponsors, date:date};
+		var sponsors = {headline:headlineSponsor},
+			date = moment().format('DD/MM/YYYY'),
+			hackEvent = 'Day 1';
+		var model = {sponsors:sponsors, hackEvent:hackEvent};
 		response.render('index', model);
 	});
 
