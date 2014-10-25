6928393b9f03cc6156f23f36f3076c982c0de434
rubygem <gemma.cameron@gmail.com>
2014-10-25 07:52:56 +0100

    should now switch automagivally

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,20 @@ module.exports = function(website){
 	website.get('/',function(request,response){
 		var headlineSponsor = {uri:'http://www.waukta.com/', name: 'WA:UK TA'}
 		var sponsors = {headline:headlineSponsor},
-			date = moment().format(),
+			date = moment(),
+			video = "true",
 			hackEvent = 'Day 1';
-		var model = {sponsors:sponsors, hackEvent:hackEvent};
+		if (date.isAfter('2014-10-26')){
+			hackEvent = 'Day 2';
+		}
+		if (date.isAfter('2014-10-26 18:00')){
+			hackEvent = 'Awards';
+		}
+		if (date.isAfter('2014-10-27')){
+			video = "false";
+		}
+		var model = {sponsors:sponsors, hackEvent:hackEvent, video:video};
+		console.log(model);
 		response.render('index', model);
 	});
 
