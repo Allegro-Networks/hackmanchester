b7b963edf5c48aa2f8f02adc8ceb9a76c2227dc3
rubygem <gemma.cameron@gmail.com>
2014-10-29 15:47:27 +0000

    adding awards show

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,7 @@ module.exports = function(website){
 	website.get('/',function(request,response){
 		var headlineSponsor = {uri:'http://www.waukta.com/', name: 'WA:UK TA'}
 		var sponsors = {headline:headlineSponsor},
-			date = moment(),
-			video = "true",
-			hackEvent = 'Day 1';
-		if (date.isAfter('2014-10-26')){
-			hackEvent = 'Day 2';
-		}
-		if (date.isAfter('2014-10-26 18:00')){
-			hackEvent = 'Awards';
-		}
-		if (date.isAfter('2014-10-27')){
-			video = "false";
-		}
+			date = moment();
 		var model = {sponsors:sponsors, hackEvent:hackEvent, video:video};
 		console.log(date.format('DD/MM/YY h:mm:ss'));
 		console.log(model);
